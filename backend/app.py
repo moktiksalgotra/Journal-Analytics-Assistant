4e5be66750e4350f417c3efc51a088f4ae4df77b
moktiksalgotra <moktik1271@gmail.com>
2026-05-04 13:53:25 +0530

    fixed syntax errors

--- backend/app.py
+++ backend/app.py
@@ -62,7 +62,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://journal-ai-backend-h2mc.onrender.com,
+        "https://journal-ai-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
