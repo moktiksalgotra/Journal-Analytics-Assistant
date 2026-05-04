d4c58f62c413fb5369cd850f266b920b77051386
moktiksalgotra <moktik1271@gmail.com>
2026-05-04 14:07:32 +0530

    fixed backend cors

--- backend/app.py
+++ backend/app.py
@@ -63,6 +63,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://journal-ai-frontend.onrender.com",
+        "https://journal-analytics-assistant.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
